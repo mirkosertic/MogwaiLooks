249e2b1156f3e3aa4bdfe3298a0696887ac9b556
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2008-01-09 20:37:49 +0000

    *** empty log message ***

---
+++ trunk/mogwai/MogwaiLooks/src/main/java/de/mogwai/common/client/looks/components/menu/DefaultRadioButtonMenuItem.java
@@ -0,0 +1,72 @@
+/**
+ * Mogwai Looks. Copyright (C) 2002 The Mogwai Project.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package de.mogwai.common.client.looks.components.menu;
+
+import java.awt.Font;
+
+import javax.swing.Action;
+import javax.swing.JRadioButtonMenuItem;
+
+import de.mogwai.common.client.looks.UIInitializer;
+import de.mogwai.common.client.looks.components.action.DefaultAction;
+import de.mogwai.i18n.I18NAble;
+import de.mogwai.i18n.ResourceHelper;
+import de.mogwai.i18n.ResourceHelperLocator;
+
+public class DefaultRadioButtonMenuItem extends JRadioButtonMenuItem implements I18NAble {
+
+	private String resourceID;
+
+	public DefaultRadioButtonMenuItem(String aResourceID) {
+
+		super(aResourceID);
+		resourceID = aResourceID;
+		initialize();
+	}
+
+	public DefaultRadioButtonMenuItem(Action aAction) {
+
+		super(aAction);
+		initialize();
+	}
+	
+	@Override
+	public void setFont(Font aFont) {
+
+		super.setFont(aFont.deriveFont(Font.PLAIN));
+	}
+
+	public String getResourceBundleID() {
+
+		return resourceID;
+	}
+
+	public ResourceHelper getResourceHelper() {
+
+		Action theAction = getAction();
+		if (theAction instanceof DefaultAction) {
+			return ((DefaultAction)theAction).getResourceHelper();
+		}
+		return ResourceHelperLocator.findResourceHelperFor(this);
+	}
+
+	private void initialize() {
+
+		UIInitializer.getInstance().initializeFontAndColors(this);
+	}
+}
